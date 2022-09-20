c2df683fa61d95576ab179db994c488414709883
y.hoshi <ysk.hoshi.1118@gmail.com>
2022-09-20 15:53:59 +0900

    format変数の削除

--- print.js
+++ print.js
@@ -3,12 +3,6 @@ module.exports.statement = function statement(invoice, plays) {
   let volumeCredits = 0;
   let result = `Statement for ${invoice.customer}`;
 
-  const format = new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
-    minimumFractionDigits: 2
-  }).format;
-
   for (let perf of Object.values(invoice.performances)) {
     volumeCredits += volumeCreditsFor(perf);
     // 注文の内訳を表示
@@ -47,9 +41,17 @@ module.exports.statement = function statement(invoice, plays) {
 
   function volumeCreditsFor(aPerformance) {
     let result = 0;
-    result += Math.max(aPerformance - 30, 0);
-    if ("comedy" === playFor(aPerformance))
+    result += Math.max(aPerformance.audience - 30, 0);
+    if ("comedy" === playFor(aPerformance).type)
       result += Math.floor(aPerformance.audience / 5);
     return result;
+  }
+
+  function format(aNumber) {
+    return new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+      minimumFractionDigits: 2
+    }).format(aNumber);
   }
 }
