af0b1791301019ea2fd41bc68ca84662535836c9
y.hoshi <ysk.hoshi.1118@gmail.com>
2022-09-22 15:41:00 +0900

    サブクラスに責務を委譲

--- createStatementData.mjs
+++ createStatementData.mjs
@@ -56,25 +56,7 @@ class PerformanceCalculator {
   }
 
   get amount() {
-    let result = 0;
-    switch (this.play.type) {
-      case "tragedy":
-        result = 40000;
-        if (this.performance.audience > 30) {
-          result += 1000 * (this.performance.audience - 30)
-        }
-        break;
-      case "comedy":
-        result = 30000;
-        if (this.performance.audience > 20) {
-          result += 10000 + 500 * (this.performance.audience - 20)
-        }
-        result += 300 * this.performance.audience;
-        break;
-      default:
-        throw new Error(`unknown type: ${playFor(this.play).type}`);
-    }
-    return result;
+    throw new Error("サブクラスの責務")
   }
 
   get volumeCredits() {
@@ -87,9 +69,22 @@ class PerformanceCalculator {
 }
 
 class TragedyCalculator extends PerformanceCalculator {
-
+  get amount() {
+    let result = 40000;
+    if (this.performance.audience > 30) {
+      result += 1000 * (this.performance.audience - 30);
+    }
+    return result;
+  }
 }
 
 class ComedyCalculator extends PerformanceCalculator {
-
+  get amount() {
+    let result = 30000;
+    if (this.performance.audience > 20) {
+      result += 10000 + 500 * (this.performance.audience - 20);
+    }
+    result += 300 * this.performance.audience;
+    return result;
+  }
 }
